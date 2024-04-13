2d6248af32aa919931d7737ec4fd6239c5022073
Yucheoru <dbcjf182529@gmail.com>
2024-04-14 01:30:17 +0900

    add_more

--- Neural Network.py
+++ Neural Network.py
@@ -113,10 +113,11 @@ print(B2.shape)
 
 A2 = np.dot(Z1, W2) + B2
 Z2 = sigmoid(A2)
+
+
 # -
 
-3. Sending signals from second layer to output-layer by using identity function
-
+# 3. Sending signals from second layer to output-layer by using identity function
 
 # +
 def identity_function(x):
@@ -127,10 +128,11 @@ B3 = np.array([0.1, 0.2])
 
 A3 = np.dot(Z2, W3) + B3
 Y = identity_function(A3)
+
+
 # -
 
-4.Total cleanup code
-
+# 4.Total cleanup code
 
 # +
 def init_network():
@@ -200,15 +202,48 @@ def softmax(a):
 # +
 import sys, os
 sys.path.append(os.pardir) # setting to import files from parent directories
+import numpy as np
+import pickle
 from dataset.mnist import load_mnist
+from PIL import  Image
 
-(x_train, t_train), (x_test, t_test) = \ # return MNIST data into form of "(train_img, train_label), (test_img, test_label)"
-    load_mnist(flatten=True, normalize=False)
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+# return MNIST data into form of "(train_img, train_label), (test_img, test_label)"
+    return x_test, t_test
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
 # -
 
 
